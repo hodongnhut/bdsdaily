137fe11cf5b39d2d08028482f2a8f65f27922423
Leo <hodongnhut@gmail.com>
2025-10-01 15:24:53 +0700

    update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -96,7 +96,7 @@ AppAsset::register($this);
             <div class="phone-vr">
                 <div class="phone-vr-circle-fill"></div>
                 <div class="phone-vr-img-circle">
-                    <a target="_blank" href="https://zalo.me/0845528 ">
+                    <a target="_blank" href="https://zalo.me/0845528145 ">
                         <img alt="Zalo" src="https://bdsdaily.com/img/zalo.png">
                     </a>
                 </div>
