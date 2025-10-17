fb097c0ec6b18ae09700801709bfd91561604c63
Leo <hodongnhut@gmail.com>
2025-10-17 15:04:20 +0700

    update

--- backend/controllers/api/ContactController.php
+++ backend/controllers/api/ContactController.php
@@ -67,9 +67,7 @@ class ContactController extends Controller
         if (!empty($keyword)) {
             $query->andFilterWhere([
                 'or',
-                ['like', 'name', $keyword],
                 ['like', 'phone', $keyword],
-                ['like', 'email', $keyword],
             ]);
         }
 
