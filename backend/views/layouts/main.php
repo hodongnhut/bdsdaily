6e61f482797618b76b66cb24eb952f762602e2d8
Leo <hodongnhut@gmail.com>
2025-11-25 13:11:05 +0700

    update check duplidate

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -62,7 +62,7 @@ AppAsset::register($this);
             </a>
             <a href="<?= \yii\helpers\Url::to(['/loan']) ?>" class="nav-item <?= Yii::$app->controller->action->id === 'loan' ? 'bg-blue-100 text-blue-600' : '' ?>" aria-label="BĐ Quy Hoạch">
                 <i class="fas fa-calculator text-xl"></i>
-                <span>Tính Toán Khoản Vay</span>
+                <span>Check Khoản Vay</span>
             </a>
             <a href="<?= \yii\helpers\Url::to(['/calendar']) ?>" class="nav-item <?= Yii::$app->controller->action->id === 'calendar' ? 'bg-blue-100 text-blue-600' : '' ?>" aria-label="BĐ Quy Hoạch">
                 <i class="fas fa-calendar text-xl"></i>
