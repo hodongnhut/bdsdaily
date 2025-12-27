618853459a9919bfe15f30055e2c388eb763ca2e
Leo <hodongnhut@gmail.com>
2025-12-27 09:44:08 +0700

    update

--- backend/config/main.php
+++ backend/config/main.php
@@ -95,6 +95,7 @@ return [
                 [
                     'class' => 'yii\rest\UrlRule',
                     'controller' => 'api/user',
+                    'pluralize' => false,
                 ],
                 'POST api/post/add-property-history/<property_id:\d+>' => 'api/post/add-property-history',
                 'GET api/post/view-property/<external_id:[\w-]+>' => 'api/post/view-property',
